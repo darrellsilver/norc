871b8ac46f9eefe7a9aa3e265ef7720d72fb1dce
Max Bogue <elnaril@gmail.com>
2010-07-30 14:54:56 -0400

    Improved FileLog in log_new.py to handle echoing to stdout and redirecting of both stdout and stderr.

--- norc_utils/log_new.py
+++ norc_utils/log_new.py
@@ -1,11 +1,15 @@
 
 """General logging utilities."""
 
+import os
 import sys
 import datetime
 import traceback
 
-from norc.settings import LOGGING_DEBUG
+from norc.settings import NORC_LOG_DIR, LOGGING_DEBUG
+
+def make_log(norc_path, debug=None):
+    return FileLog(os.path.join(NORC_LOG_DIR, norc_path), debug=debug)
 
 def timestamp():
     return datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S.%f')
@@ -13,7 +17,7 @@ def timestamp():
 class Log(object):
     """Abstract class for creating a text log."""
     
-    INFO = ' INFO'
+    INFO = 'INFO'
     ERROR = 'ERROR'
     DEBUG = 'DEBUG'
     
@@ -27,7 +31,7 @@ class Log(object):
     def info(self, msg):
         raise NotImplementedError
     
-    def error(self, msg, e):
+    def error(self, msg, trace):
         raise NotImplementedError
     
     def debug(self, msg):
@@ -37,29 +41,44 @@ class Log(object):
 class FileLog(Log):
     """Implementation of Log that sends logs to a file."""
     
-    def __init__(self, out=None, err=None, buffer=False, debug=None):
+    def __init__(self, out=None, err=None, debug=None):
         Log.__init__(self, debug)
         self.out = open(out, 'a') if out else sys.stdout
         if not err and out:
             self.err = self.out
         else:
             self.err = open(err, 'a') if err else sys.stderr
-        self.buffer = buffer
+        # Don't echo if already outputting to stdout.
+        self.echo = self.debug and out
+    
+    def __del__(self):
+        if self.out != sys.stdout:
+            self.out.close()
+        if self.err != sys.stderr:
+            self.err.close()
     
     def write(self, stream, msg):
         stream.write(msg)
-        if not self.buffer:
-            stream.flush()
+        stream.flush()
+        if self.echo:
+            print >>sys.__stdout__, msg
     
     def info(self, msg):
         self.write(self.out, Log.format(msg, Log.INFO))
     
-    def error(self, msg, e=None, st=False):
+    def error(self, msg, trace=False):
         self.write(self.err, Log.format(msg, Log.ERROR))
-        if e and st:
-            pass #print stacktrace somehow
+        if trace:
+            self.write(self.err, traceback.format_exc())
     
     def debug(self, msg):
         if self.debug:
             self.write(self.out, Log.format(msg, Log.DEBUG))
     
+    def start_redirect(self):
+        sys.stdout = self.out
+        sys.stderr = self.err
+    
+    def end_redirect(self):
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
