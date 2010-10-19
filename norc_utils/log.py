b1e854ccd2343350d04bc8a37f895a5274d92c71
Max Bogue <elnaril@gmail.com>
2010-10-19 15:39:30 -0400

    Try/catch the os.makedirs statement; this may solve a low-frequency bug in executor.

--- norc_utils/log.py
+++ norc_utils/log.py
@@ -81,7 +81,10 @@ class Log(AbstractLog):
         AbstractLog.__init__(self, debug)
         if not isinstance(log_file, file):
             if not os.path.isdir(os.path.dirname(log_file)):
-                os.makedirs(os.path.dirname(log_file))
+                try:
+                    os.makedirs(os.path.dirname(log_file))
+                except OSError:
+                    pass
             log_file = open(log_file, 'a')
         self.file = log_file
         self.echo = echo
