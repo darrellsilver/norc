470c871ac98b737e009318b264fd79005016cea6
Max Bogue <elnaril@gmail.com>
2010-11-21 18:23:08 -0500

    Good news: host requests work.  Bad news: requests don't always work...

--- cli/norc_control.py
+++ cli/norc_control.py
@@ -11,6 +11,11 @@ from optparse import OptionParser
 
 from norc.core.constants import Status, Request
 from norc.core.models import Executor, Scheduler
+from norc.norc_utils.django_extras import MultiQuerySet
+
+EXECUTOR_KEYWORDS = ["e", "executor"]
+SCHEDULER_KEYWORDS = ["s", "scheduler"]
+HOST_KEYWORDS = ["h", "host"]
 
 def main():
     usage = "norc_control [e | executor | s | scheduler] <id> " + \
@@ -34,16 +39,13 @@ def main():
         help="Send an reload request to a Scheduler.")
     parser.add_option("-f", "--force", action="store_true", default=False,
         help="Force the request to be made..")
+    parser.add_option("-w", "--wait", action="store_true", default=False,
+        help="Wait until the request has been responded to.")
     
-    (options, args) = parser.parse_args()
+    options, args = parser.parse_args()
     
     if len(args) != 2:
         bad_args("A process type and id are required.")
-    
-    try:
-        obj_id = int(args[1])
-    except ValueError:
-        bad_args("Invalid id '%s'; must be an integer." % args[1])
     
     # requests = ['stop', 'kill', 'pause', 'resume']
     requests = filter(lambda a: getattr(options, a.lower()),
@@ -51,12 +53,36 @@ def main():
     if len(requests) != 1:
         bad_args("Must only request one action at a time.")
     request = requests[0]
+    req = getattr(Request, request.upper())
+    
+    if args[0] in EXECUTOR_KEYWORDS + SCHEDULER_KEYWORDS:
+        try:
+            obj_id = int(args[1])
+        except ValueError:
+            bad_args("Invalid id '%s'; must be an integer." % args[1])
     
     cls = None
-    if args[0] in ("e", "executor"):
+    if args[0] in EXECUTOR_KEYWORDS:
         cls = Executor
-    elif args[0] in ["s", "scheduler"]:
+    elif args[0] in SCHEDULER_KEYWORDS:
         cls = Scheduler
+    elif args[0] in HOST_KEYWORDS:
+        daemons = MultiQuerySet(Executor, Scheduler).objects.all()
+        daemons = daemons.filter(host=args[1])
+        for d in daemons:
+            # print d
+            if options.force or (not Status.is_final(d.status) and
+                                d.request == None):
+                d.make_request(req)
+                print "%s was sent a %s request." % (d, request.upper())
+        if options.wait:
+            fin = lambda: all(map(lambda d:
+                Status.is_final(d.status), daemons))
+            while not fin:
+                time.sleep(0.1)
+            
+        
+        
     else:
         print "Invalid keyword '%s'." % args[0]
     
@@ -67,7 +93,6 @@ def main():
         except cls.DoesNotExist:
             print "Could not find a(n) %s with id=%s" % (name, obj_id)
         else:
-            req = getattr(Request, request.upper())
             if Status.is_final(d.status) and not options.force:
                 print "%s #%s is already in a final state." % (name, obj_id)
             elif d.request == None or options.force:
